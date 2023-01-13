eabf766407f2af5bdc7130085056d6050b6ec4f9
Stosskopf <Thomasstosskopf@gmail.com>
2023-01-13 09:07:57 +0100

    avancement dans fasta_script_reader.py

--- TP2/fasta_script_reader.py
+++ TP2/fasta_script_reader.py
@@ -4,7 +4,7 @@ import sys
 def adn_read(fastafile):
     # create a list with the letters that we consider as nucleotid
     NTP_LIST = ["A", "C", "G", "T"]
-    # creat an empty dictonnary to store the sequence and their headers
+    # create an empty dictonary to store the sequence and their headers
     sequence_dict = {}
     # open the fasta file
     with open(fastafile, "r") as fasta_file:
@@ -12,13 +12,22 @@ def adn_read(fastafile):
         line_counter = 0
         # initialize variable to keep track of the header we encounter
         header = ""
+        current_sequence = ""
         # Iterate through each line in the files (we didn't open the entire file in one variable this time)
         for line in fasta_file:
             # for each line, add +1 to the line_counter variable
             line_counter += 1
-            # if the line start with "with" we start this if statement, and it means that it's a header
+            # if the line start with ">" we start this 'if' statement, and it means that it's a header
             if line.startswith(">"):
+                # Add the line in our dictionary as a key
                 header = line.strip()
+                sequence_dict[line.strip()] = ""
+                # if the current_sequence is not empty, it means we already did at least one loop
+                if current_sequence:
+                    sequence_dict[line.strip()] = current_sequence
+                    current_sequence = ""
+
+        # return sequence_dict
             else:
                 line = line.strip()
                 line = line.upper()
@@ -26,12 +35,12 @@ def adn_read(fastafile):
                 for char in line:
                     column_counter += 1
                     if char not in NTP_LIST:
-                        print(char + " It's not a nucl in line " + str(counter) +
-                              " and column " + str(column_counter) + " for sequence " + header[1:])
-
+                        print(char + " is not a nucl in line " + str(line_counter) +
+                              " and column " + str(column_counter) + " for sequence " + header[1:] )
 
 #print(sys.argv)
 #for arg in sys.argv[1:]:
 #    adn_read(arg)
 
 
+print(adn_read("exemple.fasta"))
