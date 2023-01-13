ebe7f2c71f1aee747eae17356f5db9542af02867
Stosskopf <Thomasstosskopf@gmail.com>
2023-01-13 06:50:11 +0100

    ajout du script que l'on a fait ensemble

---
+++ select_fasta_parser.py
@@ -0,0 +1,23 @@
+import argparse
+import sys
+
+def create_parser():
+    """ Declares new parser and adds parser arguments """
+    program_description=''' reading fasta file and checking sequence format '''
+    parser =  argparse.ArgumentParser(add_help=True,description=program_description)
+    parser.add_argument('-i','--inputfile',default=sys.stdin,
+    help="required input file in fasta format",type=argparse.FileType("r"),required=True)
+    return parser
+
+def main():
+    """ Main function for reading fasta file and checking sequence format """
+    parser = create_parser()
+    args = parser.parse_args()
+    args = args.__dict__
+    print(args["inputfile"])
+    fasta_sequence = args["inputfile"].read()
+    print(fasta_sequence)
+
+
+if __name__ == "__main__":
+    main()
