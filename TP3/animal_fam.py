075bbedf1e4f7eb91cac553a902924ea70e6b5e5
Stosskopf <Thomasstosskopf@gmail.com>
2023-01-22 16:25:30 +0100

    try to many thing, i cant remeber my own name now

--- TP3/animal_fam.py
+++ TP3/animal_fam.py
@@ -5,50 +5,53 @@ We can create animals, that are define by their species, diet
 Every animal can have a parent and children
 """
 import gc
-class Animal():
+from typing import List, Dict
+
+
+class Animal:
     """
-    class animal where we define general stuff about animal
+    class Animal where we define general stuff about Animal
     """
-    def __init__(self, species, age, diet, mother = "Thomas <3") -> None:
+    def __init__(self, species: str, age: float, diet, mother="Thomas <3") -> None:
         """
         we define here attribute of our animal class
-        :param species: the species
-        :param age: age of the animal
-        :param diet: animal diet
+        :param species: the species (str)
+        :param age: age of the animal (float)
+        :param diet: animal diet (str?)
         """
-        self.species = species
-        self.age = age
-        self.diet = diet
-        self.children = [None]
-        self.mother = mother
-        # a list of the id of the variable in memory
-        self.child_id = []
-        self.descendent = [None]
+        self.species: str = species
+        self.age: float = age
+        self.diet: str = diet
+        self.children: List[str] = []
+        self.mother: str = mother
+        self.child_id: List = []
+        """child_id is a list of the id of the variable in memory"""
+        self.descendent: List = []
 
     def __str__(self) -> str:
         """if we do a print on animal, it will print some animal's features"""
-        return "Species : " + self.species + "\n" \
-            +"age : " + str(self.age) + "\n" \
-            +"diet : "+ self.diet + "\n"\
-            +"Mother : " + self.mother + "\n"\
-            + "Children : " + str(self.children) + "\n"\
-            + "child memory path : " + str(self.child_id) +"\n"\
-            + "Descendent : " + str(self.descendent)
+        return f"Species : {self.species} \n" \
+               f"age: {self.age}\n"\
+               f"diet: {self.diet}\n"\
+               f"Mother: {self.mother}\n"\
+               f"Children: {str(self.children)}\n"\
+               f"child memory path:  {str(self.child_id)}\n"\
+               f"Descendent: {str(self.descendent)}"
 
     def add_children(self, child):
-        """function to add child to an animal"""
-        # In this function, child is an object,
-        # We take the attribute name from or object
-        # and we put this attribute in our list of children
-        if self.children[0] is None:
-            self.children.remove(None)
+        """
+        add_children() adds child (of class Animal) to an Animal.
+        In this function, child is an object,
+        and we put this attribute in our list of children
+        We take the attribute name from or object.
+        """
         self.children.append(child.name)
-        self.child_id.append(id(child))
+        self.child_id.append(child)
         self.add_mother(child,self)
-        self.add_descendent()
+        self.add_descendent(child)
 
-    def add_mother(self,child ,mommy):
-        """function to add a mother
+    def add_mother(self, child, mommy):
+        """add_mother adds a mother to an Animal object.
         for that purpose, we iterate through all
         the children in children list add change
         the name of their attribute mother
@@ -57,19 +60,54 @@ class Animal():
             if element == child.name:
                 child.mother = mommy.name
 
-    def add_descendent(self):
+    def add_descendent(self, child):
         """function to add descendent in the list"""
 
-        if self.descendent[0] is None:
-            self.descendent.remove(None)
-        for elem in self.child_id:
-            for kids in self.children:
-                self.descendent.append(kids)
-                #self.descendent.append(elem)
-                self.descendent.append(__class__.add_descendent(elem))
 
+        for index in range(len(self.child_id)):
+            self.descendent.append(self.children[index])
+            self.descendent.append(child.add_descendent(child))
         return self.descendent
 
+    # def retrieve_children(self, input_animal) -> None:
+    #     """ retrieve_children returns all children object into a List"""
+    #     descendent = []
+    #     children = input_animal.descendent
+    #     for child in children:
+    #         pass
+    #         # descendent.append(child.)
+    #     flag = True
+    #     while flag:
+    #         for child in children:
+    #             descendent.append(child)
+    #     # animal -> [child1,child2]
+    #     #     -> child1 -> [child12, child13]
+    #     for child in children:
+    #         descendent.append(child)
+    #         foobar = child.descendent
+    #         while len(foobar) < 1:
+    #             descendent.append(*child.descendent)
+    #             pass # foobar[:]
+
+
+    # def get_children(self):
+    #     return self.descendent
+    #
+    # def get_descendence_list(self, animal):
+    #     first_level_children = animal.descendent
+    #     children_list = [first_level_children]
+    #     while len(children_list)>1:
+    #         children_list.append(*self.get_descendence_list(animal))
+    #         pass # first_level_children
+    #     return children_list
+# Function for nth Fibonacci number
+
+
+# Driver Program
+# print(Fibonacci(9))
+
+# This code is contributed by Saket Modi
+# then corrected and improved by Himanshu Kanojiya
 
  # """
  #        Recursively adds all descendants of the animal to a list
@@ -128,23 +166,69 @@ class Dog(Animal):
             + "\nname : " + self.name +"\n"+\
             "______________________\n"+ super().__str__()
 
+    # def retrieve_children(self, input_animal: Animal) -> None:
+    #     """ retrieve_children returns all children object into a List"""
+    #     children = input_animal.descendent
+    #     self.descendent = children
+
+
 # TEST HERE
 
 if __name__ == "__main__":
-    Dog0 = Dog(10, "Boby")
-    Dog1 = Dog(8, "Martel")
-    Dog3 = Dog(8, "Hakam")
-    Dog4 = Dog(7, "Joelle")
-    Dog5 = Dog(5, "Patate")
+    Dog0 = Dog(age=10,name= "Boby")
+    Dog1 = Dog(age=8, name="Martel")
+    Dog3 = Dog(age=8, name="Hakam")
+    Dog4 = Dog(age=7, name="Joelle")
+    Dog5 = Dog(age=5, name="Patate")
     Dog0.add_children(Dog3)
     Dog0.add_children(Dog1)
     Dog1.add_children(Dog5)
-    print(Dog0.add_descendent())
+    print(Dog0)
     print(Dog1)
     print(Dog3)
     print(Dog5)
 
+    # bob = {
+    #     'mother': ['thomas'],
+    #     'child': []
+    # }
+    #
+    # francis = {
+    #     'mother': ['thomas'],
+    #     'child': [bob]
+    # }
+    #
+    # lucas = {
+    #     'mother': ['thomas'],
+    #     'child': [francis]
+    # }
 
+    # def return_all_fib(n=15):
+    #     def Fibonacci(n):
+    #         # Check if input is 0 then it will print incorrect input
+    #         if n < 0:
+    #             print("Incorrect input")
+    #         # Check if n is 0 then it will return 0
+    #         elif n == 0:
+    #             return 0
+    #         # Check if n is 1,2 it will return 1
+    #         elif n == 1 or n == 2:
+    #             return 1
+    #         else:
+    #             return Fibonacci(n - 1) + Fibonacci(n - 2)
+    #     return [Fibonacci()]
+
+    # def get_child(dog: Dict):
+    #     if dog['child']
+    #     return dog['child']
+    # dogs = []
+    # while new_children is not None:
+    #     dogs.append()
+    #     new_children = get_child(child)
+    #     return [dog.get_children()]
+    # def get_child(dog):
+    #         return []
+    #     else
 
 
     # pouvoir ajouter un enfant dans un animal
