2227e5b4f3b815f8b76d14a9041866a0d8d8fb4b
Stosskopf <Thomasstosskopf@gmail.com>
2023-02-15 08:09:49 +0100

    add some description message in animal.py

--- TP6/animal.py
+++ TP6/animal.py
@@ -1,7 +1,19 @@
 # encoding : utf8
 
-class Animal():
+class Animal:
+    """
+    Animal is defined by 5 parameters:
+    species, age, diet, foot, and name. each animal wil be saved in a file.
+    """
     def __init__(self, species, age, diet, foot, name):
+        """
+        method to define animal's parameters
+        :param species: string
+        :param age: int
+        :param diet: string
+        :param foot: int
+        :param name: string
+        """
         self.species = species
         self.age = age
         self.diet = diet
@@ -9,6 +21,7 @@ class Animal():
         self.name = name
 
     def __str__(self):
+        """what will be return if we print an animal"""
         return self.species +"/"+str(self.age) +"/"+self.diet +"/"+str(self.foot) +"/"+self.name
 
 
